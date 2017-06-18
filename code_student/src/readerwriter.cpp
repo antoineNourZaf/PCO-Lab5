26b978e7a0c389cb00c286bfd734b85960656967
dbnsky <emmanuel.schmid@gmail.com>
2017-06-18 22:25:56 +0200

    debug -test

--- code_student/src/readerwriter.cpp
+++ code_student/src/readerwriter.cpp
@@ -1,6 +1,31 @@
 #include "abstractreaderwriter.h"
 
+AbstractReaderWriter::AbstractReaderWriter(){
+
+}
+
+AbstractReaderWriter::~AbstractReaderWriter(){
+
+}
+
+ReaderWriterSemaphore::~ReaderWriterSemaphore(){
+}
 
 ReaderWriterSemaphore::ReaderWriterSemaphore(){
+}
+
+void ReaderWriterSemaphore::lockReading(){
+
+}
+
+void ReaderWriterSemaphore::lockWriting(){
+
+}
+
+void ReaderWriterSemaphore::unlockReading(){
+
+}
+
+void ReaderWriterSemaphore::unlockWriting(){
 
 }
