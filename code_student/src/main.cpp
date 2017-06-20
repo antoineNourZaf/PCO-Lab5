4c64821923e4b914e41cdfd385d4bffd1747300f
Camilo PINEDA SERNA <camilo.pinedaserna@heig-vd.ch>
2017-06-20 17:54:00 +0200

    Ajout GITIGNORE et entete MAIN

--- code_student/src/main.cpp
+++ code_student/src/main.cpp
@@ -6,6 +6,24 @@
  * @author Emmanuel SCHMID
  *
  * Voici notre implementation du laboratoire :
+ *
+ * Le but est d'avoir divers treads qui implémentent différents protocoles de lecteurs-redacteurs.
+ * Nous avons voulu implémenter une version avec priorité aux rédacteurs
+ * et une avec priorité égale pour les lecteurs comme pour les rédacteurs.
+ *
+ * Chacun de ses protocoles peut être implémenté grâce à différents objets de synchronisation :
+ * Sémpahores, moniteur de Hoare et moniteur de Mesa.
+ *
+ * La classe AbstractReaderWriter va être dérivée pour chacun des protocoles implémenté avec chacun des objets de synchronisation.
+ * Dans cette dernière sont définits tous les protocoles (comme ReaderWriterSemaphoreEqualPrio).
+ * Leur implémentation est dans le fichier readerwriter.cpp
+ *
+ * Nous devons également "wrapper" les objets de synchronisation offerts par le langage.
+ * Ainsi, QSemaphore, QMutex et QWaitCondition deviennent OSemaphore, OMutex et OWaitCondition.
+ * Ces derniers vont être monitorés, c'est-à-dire qu'en plus de fonctionner comme objets de synchronisation
+ * ils permettront d'afficher des messages et d'interagir avec l'utilisateur.
+ * Cette interaction et monitoring se fait grâce aux classes WaitingLogger et SynchroController.
+ *
  */
 #include <QApplication>
 #include <iostream>
