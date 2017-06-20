3e1758d9ea96f223a4d9da966263b308d2ce14b0
Camilo PINEDA SERNA <camilo.pinedaserna@heig-vd.ch>
2017-06-20 20:09:32 +0200

    fin comments

--- code_student/src/synchrocontroller.cpp
+++ code_student/src/synchrocontroller.cpp
@@ -1,3 +1,13 @@
+/**
+ *  PCO Labo 5 - Monitoring de concurrence
+ *
+ * @author Antoine NOURAZAR
+ * @author Camilo PINEDA SERNA
+ * @author Emmanuel SCHMID
+ *
+ * @file synchrocontroller.cpp
+ *
+ */
 #include "synchrocontroller.h"
 
 SynchroController::SynchroController()
