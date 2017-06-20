66310401246a84d15216432892bf6474d70e8877
Camilo PINEDA SERNA <camilo.pinedaserna@heig-vd.ch>
2017-06-20 19:24:05 +0200

    pre pull

--- code_student/src/omesamonitor.cpp
+++ code_student/src/omesamonitor.cpp
@@ -1,3 +1,14 @@
+/**
+ *  PCO Labo 5 - Monitoring de concurrence
+ *
+ * @author Antoine NOURAZAR
+ * @author Camilo PINEDA SERNA
+ * @author Emmanuel SCHMID
+ *
+ * @file omesamonitor.cpp
+ *
+ * Ce fichier définit le classe OMesaMonitor.
+ */
 #include "omesamonitor.h"
 
 OMesaMonitor::OMesaMonitor()
--- code_student/src/omutex.h
+++ code_student/src/omutex.h
@@ -1,3 +1,16 @@
+/**
+ *  PCO Labo 5 - Monitoring de concurrence
+ *
+ * @author Antoine NOURAZAR
+ * @author Camilo PINEDA SERNA
+ * @author Emmanuel SCHMID
+ *
+ * @file omutex.h
+ *
+ * Ce fichier définit le classe omutex.
+ * Cet objet de synchronisation sera augmenté de méthodes de logging (add/remove-RessourceAcces)
+ *
+ */
 #ifndef PMUTEX_H
 #define PMUTEX_H
 
